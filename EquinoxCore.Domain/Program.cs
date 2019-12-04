d7d2224020b49cff06fa381111582fc7b7521c11
Qi Xiang <qixyqix@outlook.com>
2019-12-04 23:03:37 +0800

    Added Web and Domain Projects

---
+++ EquinoxCore.Domain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EquinoxCore.Domain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
