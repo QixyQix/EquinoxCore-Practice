5c647e68adbfc4ff4e559555d458f64ed418b176
Qi Xiang <qixyqix@outlook.com>
2019-12-07 16:08:30 +0800

    Added StoredEvent class

---
+++ EquinoxCore.Domain.Core/Event/StoredEvent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquinoxCore.Domain.Core.Event
+{
+    public class StoredEvent : Event
+    { 
+        public Guid Id { get; private set;}
+
+        public string Data { get; private set; }
+
+        public string User { get; private set; }
+
+        //EF Constructor
+        protected StoredEvent() { }
+
+        public StoredEvent(Event theEvent, string data, string user)
+        {
+            Id = Guid.NewGuid();
+            AggregateId = theEvent.AggregateId;
+            MessageType = theEvent.MessageType;
+            Data = data;
+            User = user;
+        }
+    }
+}
