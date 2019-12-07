f4f15a3fa5d4deaae72af53968434c6ee0c7f293
Qi Xiang <qixyqix@outlook.com>
2019-12-07 16:35:22 +0800

    Added Event/Command Bus Mediator Handler Interface

---
+++ EquinoxCore.Domain.Core/Bus/IMediatorHandler.cs
@@ -0,0 +1,15 @@
+﻿using EquinoxCore.Domain.Core.Commands;
+using EquinoxCore.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquinoxCore.Domain.Core.Bus
+{
+    interface IMediatorHandler
+    {
+        Task SendCommand<T>(T command) where T : Command;
+        Task RaiseEvent<T>(T @event) where T : Event;
+    }
+}
