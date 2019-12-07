769b1593f1acfeaafbeee985c3335bedebc891af
Qi Xiang <qixyqix@outlook.com>
2019-12-07 18:34:41 +0800

    Domain: Added Customer Model

---
+++ EquinoxCore.Domain/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using EquinoxCore.Domain.Core.Models;
+
+namespace EquinoxCore.Domain.Models
+{
+
+    class Customer : Entity
+    {
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public DateTime DateOfBirth { get; private set; }
+
+        //Empty constructor for EF
+        protected Customer() { }
+
+        public Customer(Guid id, string name, string email, DateTime birthDate) {
+            Id = id;
+            Name = name;
+            Email = email;
+            DateOfBirth = birthDate;
+        }
+
+    }
+}
