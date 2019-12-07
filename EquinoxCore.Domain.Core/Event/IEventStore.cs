3b8cb8caf83a10ad5b5cd554bce7ae154291c1a3
Qi Xiang <qixyqix@outlook.com>
2019-12-07 16:08:22 +0800

    Added EventStore and Handler Interfaces

---
+++ EquinoxCore.Domain.Core/Event/IEventStore.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquinoxCore.Domain.Core.Event
+{
+    interface IEventStore
+    {
+        void Save<T>(T theEvent) where T : Event;
+    }
+}
---
+++ EquinoxCore.Domain.Core/Event/IHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquinoxCore.Domain.Core.Event
+{
+    public interface IHandler<T> where T : Message
+    {
+        void Handle(T message);
+    }
+}
