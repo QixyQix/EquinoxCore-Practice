1aa6cf6704c4a2e0467878b09fbb48b657affb7c
Qi Xiang <qixyqix@outlook.com>
2019-12-07 15:40:57 +0800

    Added Message Class & MediatR class

---
+++ EquinoxCore.Domain.Core/Event/Message.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+
+namespace EquinoxCore.Domain.Core.Event
+{
+    public abstract class Message : IRequest<bool>
+    {
+
+    }
+}
