cc65b4e82c0c5f25f3b8333ac362ada082831bdc
Qi Xiang <Qixyqix@outlook.com>
2019-12-11 16:10:01 +0800

    EquinoxCore.Domain Customer Command & Validation

---
+++ EquinoxCore.Domain/Commands/CustomerCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using EquinoxCore.Domain.Core.Commands;
+
+namespace EquinoxCore.Domain.Commands
+{
+    public abstract class CustomerCommand : Command
+    {
+        public Guid Id { get; protected set; }
+        public string Name { get; protected set; }
+        public string Email { get; protected set; }
+        public DateTime BirthDate { get; protected set; }
+    }
+}
