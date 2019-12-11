33514673dc5bc369cd8efc2388de57da43bf141a
Qi Xiang <qixyqix@outlook.com>
2019-12-11 21:45:01 +0800

    EquinoxCore.Domain Added UnitOfWork Interface

---
+++ EquinoxCore.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquinoxCore.Domain.Interfaces
+{
+    interface IUnitOfWork : IDisposable
+    {
+        bool Commit();
+    }
+}
