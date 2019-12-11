5e68d293cce42af6e340ee225eb474cfc851bcaa
Qi Xiang <qixyqix@outlook.com>
2019-12-11 21:19:35 +0800

    EquinoxCore.Domain Added Customer Removed and Updated Events

---
+++ EquinoxCore.Domain/Events/CustomerRemovedEvent.cs
@@ -0,0 +1,18 @@
+﻿using EquinoxCore.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquinoxCore.Domain.Events
+{
+    class CustomerRemovedEvent : Event
+    {
+        public Guid Id { get; set; }
+
+        public CustomerRemovedEvent(Guid id)
+        {
+            Id = id;
+            AggregateId = id;
+        }
+    }
+}
---
+++ EquinoxCore.Domain/Events/CustomerUpdatedEvent.cs
@@ -0,0 +1,24 @@
+﻿using EquinoxCore.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquinoxCore.Domain.Events
+{
+    class CustomerUpdatedEvent : Event
+    {
+        public Guid Id { get; set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public DateTime BirthDate { get; private set; }
+
+        public CustomerUpdatedEvent(Guid id, string name, string email, DateTime birthDate)
+        {
+            Id = id;
+            AggregateId = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+        }
+    }
+}
