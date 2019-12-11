e784cac830155874f6f6935a7fa197333b140e01
Qi Xiang <Qixyqix@outlook.com>
2019-12-11 16:42:05 +0800

    EquinoxCore.Doman Added RemoveCustomer Command & Validation

---
+++ EquinoxCore.Domain/Commands/RemoveCustomerCommand.cs
@@ -0,0 +1,20 @@
+﻿using EquinoxCore.Domain.Validations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquinoxCore.Domain.Commands
+{
+    class RemoveCustomerCommand : CustomerCommand
+    {
+        public RemoveCustomerCommand(Guid id)
+        {
+            Id = id;
+        }
+        public override bool IsValid()
+        {
+            ValidationResult = new RemoveCustomerCommandValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+}
---
+++ EquinoxCore.Domain/Validations/RemoveCustomerCommandValidation.cs
@@ -0,0 +1,14 @@
+﻿using EquinoxCore.Domain.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquinoxCore.Domain.Validations
+{
+    class RemoveCustomerCommandValidation : CustomerValidation<RemoveCustomerCommand>
+    {
+        public RemoveCustomerCommandValidation() {
+            ValidateId();
+        }
+    }
+}
