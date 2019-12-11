839e18428765aaa671fbfc3e27416555d9eec989
Qi Xiang <Qixyqix@outlook.com>
2019-12-11 17:25:18 +0800

    EquinoxCore.Domain Added CustomerRegisteredEvent

---
+++ EquinoxCore.Domain/Events/CustomerRegisteredEvent.cs
@@ -0,0 +1,23 @@
+﻿using EquinoxCore.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquinoxCore.Domain.Events
+{
+    class CustomerRegisteredEvent : Event
+    {
+        public Guid Id { get; set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public DateTime BirthDate { get; private set; }
+
+        public CustomerRegisteredEvent(Guid id, string name, string email, DateTime birthDate) {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+        }
+
+    }
+}
