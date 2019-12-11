9fb1a16cb76098a5312f6cb960e8c38ea28f0953
Qi Xiang <qixyqix@outlook.com>
2019-12-12 00:19:07 +0800

    EquinoxCore.Domain Added CustomerEventHandler

---
+++ EquinoxCore.Domain/EventHandlers/CustomerEventHandler.cs
@@ -0,0 +1,37 @@
+﻿using EquinoxCore.Domain.Events;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EquinoxCore.Domain.EventHandlers
+{
+    public class CustomerEventHandler :
+        INotificationHandler<CustomerRegisteredEvent>,
+        INotificationHandler<CustomerRemovedEvent>,
+        INotificationHandler<CustomerUpdatedEvent>
+    {
+        public Task Handle(CustomerRegisteredEvent message, CancellationToken cancellationToken)
+        {
+            //Send some notificaiton Email
+
+            return Task.CompletedTask;
+        }
+
+        public Task Handle(CustomerRemovedEvent message, CancellationToken cancellationToken)
+        {
+            //Send sad to see you go email
+
+            return Task.CompletedTask;
+        }
+
+        public Task Handle(CustomerUpdatedEvent message, CancellationToken cancellationToken)
+        {
+            //Send notification email
+
+            return Task.CompletedTask;
+        }
+    }
+}
