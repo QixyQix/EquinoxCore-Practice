853b8f68761388d5753a2de17413bd130858930c
Qi Xiang <qixyqix@outlook.com>
2019-12-11 21:41:07 +0800

    EquinoxCore.Domain Added Customer Repository Interface

---
+++ EquinoxCore.Domain/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿using EquinoxCore.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquinoxCore.Domain.Interfaces
+{
+    interface ICustomerRepository : IRepository<Customer>
+    {
+        Customer GetByEmail(string email);
+    }
+}
