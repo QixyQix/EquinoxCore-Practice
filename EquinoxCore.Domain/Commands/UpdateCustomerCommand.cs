1015f2843d1dc3ed30dc2e11b54432ef886e048b
Qi Xiang <Qixyqix@outlook.com>
2019-12-11 16:46:59 +0800

    EquinoxCore.Domain Added UpdateCustomer Command & Validation

---
+++ EquinoxCore.Domain/Commands/UpdateCustomerCommand.cs
@@ -0,0 +1,22 @@
+﻿using EquinoxCore.Domain.Validations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquinoxCore.Domain.Commands
+{
+    class UpdateCustomerCommand : CustomerCommand
+    {
+        public UpdateCustomerCommand(Guid id, string name, string email, DateTime birthDate) {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+        }
+        public override bool IsValid()
+        {
+            ValidationResult = new UpdateCustomerCommandValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+}
---
+++ EquinoxCore.Domain/Validations/UpdateCustomerCommandValidation.cs
@@ -0,0 +1,17 @@
+﻿using EquinoxCore.Domain.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquinoxCore.Domain.Validations
+{
+    class UpdateCustomerCommandValidation : CustomerValidation<UpdateCustomerCommand>
+    {
+        public UpdateCustomerCommandValidation() {
+            ValidateId();
+            ValidateName();
+            ValidateEmail();
+            ValidateBirthDate();
+        }
+    }
+}
