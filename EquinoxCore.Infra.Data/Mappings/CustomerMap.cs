31f8bad768971ee5931c66fe3d2b854d36e76b27
Qi Xiang <Qixyqix@outlook.com>
2019-12-12 14:52:48 +0800

    EquinoxCore.Infra.Data Added Customer table mapping

---
+++ EquinoxCore.Infra.Data/Mappings/CustomerMap.cs
@@ -0,0 +1,28 @@
+﻿using EquinoxCore.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquinoxCore.Infra.Data.Mappings
+{
+    class CustomerMap : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.Property(c => c.Id)
+                .HasColumnName("Id");
+
+            builder.Property(c => c.Name)
+                .HasColumnType("varchar(100)")
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(c => c.Email)
+                .HasColumnName("varchar(100)")
+                .HasMaxLength(100)
+                .IsRequired();
+        }
+    }
+}
