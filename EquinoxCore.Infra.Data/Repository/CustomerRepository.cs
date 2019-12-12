5c616a99abc53600aa2fabc3d9337e7925335a5d
Qi Xiang <Qixyqix@outlook.com>
2019-12-12 15:59:39 +0800

    EquinoxCore.Infra.Data Added Customer Repository

---
+++ EquinoxCore.Infra.Data/Repository/CustomerRepository.cs
@@ -0,0 +1,18 @@
+﻿using EquinoxCore.Domain.Interfaces;
+using EquinoxCore.Domain.Models;
+using EquinoxCore.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace EquinoxCore.Infra.Data.Repository
+{
+    public class CustomerRepository : Repository<Customer>, ICustomerRepository
+    {
+        public CustomerRepository(EquinoxContext context) : base(context) { }
+
+        public Customer GetByEmail(string email)
+        {
+            return DbSet.AsNoTracking().FirstOrDefault(c => c.Email == email);
+        }
+    }
+}
