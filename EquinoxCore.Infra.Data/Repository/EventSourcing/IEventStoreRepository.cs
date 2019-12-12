3dc8b8efd4b878514295d337ad64d16632fc2f47
Qi Xiang <Qixyqix@outlook.com>
2019-12-12 16:36:11 +0800

    EquinoxCore.Infra.Data added eventstorerepository interface

---
+++ EquinoxCore.Infra.Data/Repository/EventSourcing/IEventStoreRepository.cs
@@ -0,0 +1,12 @@
+﻿using EquinoxCore.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+
+namespace EquinoxCore.Infra.Data.Repository.EventSourcing
+{
+    public interface IEventStoreRepository : IDisposable
+    {
+        void Store(StoredEvent theEvent);
+        IList<StoredEvent> All(Guid aggregateId);
+    }
+}
