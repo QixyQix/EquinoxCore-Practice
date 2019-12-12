586953f9168b8461566422ab739e146aa11f44ad
Qi Xiang <Qixyqix@outlook.com>
2019-12-12 15:18:06 +0800

    EquinoxCore.Infra.Data Added storedevent map

---
+++ EquinoxCore.Infra.Data/Mappings/StoredEventMap.cs
@@ -0,0 +1,22 @@
+﻿using EquinoxCore.Domain.Core.Events;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquinoxCore.Infra.Data.Mappings
+{
+    public class StoredEventMap : IEntityTypeConfiguration<StoredEvent>
+    {
+        public void Configure(EntityTypeBuilder<StoredEvent> builder)
+        {
+            builder.Property(se => se.Timestamp)
+                .HasColumnName("CreationDate");
+
+            builder.Property(se => se.MessageType)
+                .HasColumnName("Action")
+                .HasColumnType("varchar(100)");
+        }
+    }
+}
