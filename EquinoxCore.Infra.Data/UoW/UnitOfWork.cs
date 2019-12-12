10352f3eeb3b2efa0a6664486c1992f3d8d0f248
Qi Xiang <Qixyqix@outlook.com>
2019-12-12 17:23:35 +0800

    EquinoxCore.Infra.Data added Unit of Work

---
+++ EquinoxCore.Infra.Data/UoW/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using EquinoxCore.Domain.Interfaces;
+using EquinoxCore.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquinoxCore.Infra.Data.UoW
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        protected readonly EquinoxContext _context;
+
+        public UnitOfWork(EquinoxContext context)
+        {
+            _context = context;
+        }
+        public bool Commit()
+        {
+            return _context.SaveChanges() > 0;
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
