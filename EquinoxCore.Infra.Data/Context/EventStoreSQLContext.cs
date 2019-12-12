c0aa32df7a855d91ee2cca67342c92a2a168ace1
Qi Xiang <Qixyqix@outlook.com>
2019-12-12 15:33:46 +0800

    EquinoxCore.Infra.Data Added EventStore Context

---
+++ EquinoxCore.Infra.Data/Context/EventStoreSQLContext.cs
@@ -0,0 +1,41 @@
+﻿using EquinoxCore.Domain.Core.Events;
+using EquinoxCore.Infra.Data.Mappings;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquinoxCore.Infra.Data.Context
+{
+    class EventStoreSQLContext : DbContext
+    {
+        public DbSet<StoredEvent> StoredEvent { get; set; }
+        private readonly IHostingEnvironment _env;
+
+        public EventStoreSQLContext(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new StoredEventMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // get the configuration from the app settings
+            var config = new ConfigurationBuilder()
+                .SetBasePath(_env.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // define the database to use
+            optionsBuilder.UseNpgsql(config.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
