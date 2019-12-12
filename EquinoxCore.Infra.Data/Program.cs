d342de1512064f133eff45931180f15326c8f2b6
Qi Xiang <Qixyqix@outlook.com>
2019-12-12 14:52:15 +0800

    EquinoxCore.Infra.Data Added data infrastructure project

---
+++ EquinoxCore.Infra.Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EquinoxCore.Infra.Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
