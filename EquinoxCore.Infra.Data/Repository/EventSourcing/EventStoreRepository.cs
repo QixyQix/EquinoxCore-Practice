a4882143550c22794da5327d97cf510657fae178
Qi Xiang <Qixyqix@outlook.com>
2019-12-12 16:36:25 +0800

    EquinoxCore.Infra.Data Added EventStore Repository

---
+++ EquinoxCore.Infra.Data/Repository/EventSourcing/EventStoreRepository.cs
@@ -0,0 +1,35 @@
+﻿using EquinoxCore.Domain.Core.Events;
+using EquinoxCore.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EquinoxCore.Infra.Data.Repository.EventSourcing
+{
+    public class EventStoreRepository : IEventStoreRepository
+    {
+        protected readonly EventStoreSQLContext _context;
+
+        public EventStoreRepository(EventStoreSQLContext context)
+        {
+            _context = context;
+        }
+
+        public IList<StoredEvent> All(Guid aggregateId)
+        {
+            return (from e in _context.StoredEvent where e.AggregateId == aggregateId select e).ToList();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public void Store(StoredEvent theEvent)
+        {
+            _context.StoredEvent.Add(theEvent);
+            _context.SaveChanges();
+        }
+    }
+}
