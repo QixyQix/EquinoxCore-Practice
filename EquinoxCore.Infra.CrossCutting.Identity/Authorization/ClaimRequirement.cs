f9684bf2d5125a8c38ddf65f143732d2620d75c8
Qi Xiang <qixyqix@outlook.com>
2019-12-14 16:22:35 +0800

    EquinoxCore.Infra.CrossCutting.Identity Added ClaimRequirement & ClaimReuqirementHandler

---
+++ EquinoxCore.Infra.CrossCutting.Identity/Authorization/ClaimRequirement.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquinoxCore.Infra.CrossCutting.Identity.Authorization
+{
+    public class ClaimRequirement : IAuthorizationRequirement
+    {
+        public string ClaimName { get; set; }
+        public string ClaimValue { get; set; }
+
+        public ClaimRequirement(string claimName, string claimValue)
+        {
+            ClaimName = claimName;
+            ClaimValue = ClaimValue;
+        }
+    }
+}
---
+++ EquinoxCore.Infra.CrossCutting.Identity/Authorization/ClaimRequirementHandler.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquinoxCore.Infra.CrossCutting.Identity.Authorization
+{
+    public class ClaimRequirementHandler : AuthorizationHandler<ClaimRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ClaimRequirement requirement)
+        {
+            var claim = context.User.Claims.FirstOrDefault(c => c.Type == requirement.ClaimName);
+
+            if (claim != null && claim.Value.Contains(requirement.ClaimValue))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
