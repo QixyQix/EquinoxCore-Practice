d1179b0f51546acb7704aa6c1af832616767bb69
Qi Xiang <qixyqix@outlook.com>
2019-12-14 16:36:06 +0800

    EquinoxCore.Infra.CrossCutting.Identity Added the MessageServices class & interfaces

---
+++ EquinoxCore.Infra.CrossCutting.Identity/Services/MessageServices.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquinoxCore.Infra.CrossCutting.Identity.Services
+{
+    public class EmailMessageSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            //Plug in email service here to send email
+            return Task.FromResult(0);
+        }
+    }
+
+    public class SmsMessageSender : ISmsSender
+    {
+        public Task SendSmsAsync(string number, string message)
+        {
+            //Plug in sms service here to send sms
+            return Task.FromResult(0);
+        }
+    }
+}
