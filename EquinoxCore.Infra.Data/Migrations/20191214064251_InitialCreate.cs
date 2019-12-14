42134356964d4a7d551190cf93fb6e5bc89d6134
Qi Xiang <qixyqix@outlook.com>
2019-12-14 15:25:21 +0800

    EquinoxCore.Infra.Data Database Migration for EquinoxContext & EventStoreSqlContext

---
+++ EquinoxCore.Infra.Data/Migrations/20191214064251_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EquinoxCore.Infra.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    varchar100 = table.Column<string>(name: "varchar(100)", maxLength: 100, nullable: false),
+                    DateOfBirth = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
