8f51588ccba64e7129829be19d4d4e317ceb168c
Qi Xiang <qixyqix@outlook.com>
2019-12-14 15:25:42 +0800

    EquinoxCore.Infra.CrossCutting.Bus Added InMemoryBus

---
+++ EquinoxCore.Infra.CrossCutting.Bus/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EquinoxCore.Infra.CrossCutting.Bus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
