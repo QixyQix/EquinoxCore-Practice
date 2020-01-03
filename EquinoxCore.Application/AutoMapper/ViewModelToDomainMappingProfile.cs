0bdca9d40399863af5e4dbb1161058394b6549a7
Qi Xiang <qixyqix@outlook.com>
2020-01-04 03:14:23 +0800

    EquinoxCore.Application Added automapper ViewModelToDomainMappingProfile

---
+++ EquinoxCore.Application/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using EquinoxCore.Application.ViewModels;
+using EquinoxCore.Domain.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EquinoxCore.Application.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public ViewModelToDomainMappingProfile() {
+            CreateMap<CustomerViewModel, RegisterNewCustomerCommand>()
+                .ConstructUsing(c => new RegisterNewCustomerCommand(c.Name, c.Email, c.BirthDate));
+            CreateMap<CustomerViewModel, UpdateCustomerCommand>()
+                .ConstructUsing(c => new UpdateCustomerCommand(c.Id, c.Name, c.Email, c.BirthDate));
+        }
+    }
+}
