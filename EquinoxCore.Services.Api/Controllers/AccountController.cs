22fc08164ac053358149e919f3062cafe41f359a
Qi Xiang <Qixyqix@outlook.com>
2020-01-08 15:53:53 +0800

    EquinoxCore.Services.Api.Controller Added AccountController

---
+++ EquinoxCore.Services.Api/Controllers/AccountController.cs
@@ -0,0 +1,71 @@
+﻿using EquinoxCore.Domain.Core.Bus;
+using EquinoxCore.Domain.Core.Notifications;
+using EquinoxCore.Infra.CrossCutting.Identity.Models;
+using EquinoxCore.Infra.CrossCutting.Identity.Models.AccountViewModels;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace EquinoxCore.Services.Api.Controllers
+{
+    [Authorize]
+    public class AccountController : ApiController
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ILogger _logger;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, INotificationHandler<DomainNotification> notifications, ILoggerFactory loggerFactory, IMediatorHandler mediator) : base(notifications, mediator) {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = loggerFactory.CreateLogger<AccountController>();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("account")]
+        public async Task<IActionResult> Login([FromBody] LoginViewModel model) {
+            if (!ModelState.IsValid) {
+                NotifyModelStateErrors();
+                return Response(model);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, true);
+            if (!result.Succeeded)
+                NotifyError(result.ToString(), "Login Failure");
+
+            _logger.LogInformation(1,"User " + model.Email + " logged in.");
+            return Response(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("account/register")]
+        public async Task<IActionResult> Register(RegisterViewModel model) {
+            if (!ModelState.IsValid) {
+                NotifyModelStateErrors();
+                return Response(model);
+            }
+
+            var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded) {
+                await _userManager.AddClaimAsync(user, new Claim("Customers", "Write"));
+                _logger.LogInformation(3, "User " + model.Email + " created with password.");
+                return Response(model);
+            }
+
+            AddIdentityErrors(result);
+            return Response(model);
+        }
+    }
+}
