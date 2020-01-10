c75891c5a6f9f7067032fc25539c3ed90a7d42d1
Qi Xiang <Qixyqix@outlook.com>
2020-01-10 11:11:04 +0800

    EquinoxCore.Web Added SummaryViewComponent

---
+++ EquinoxCore.Web/ViewComponents/SummaryViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using EquinoxCore.Domain.Core.Notifications;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EquinoxCore.Web.ViewComponents
+{
+    public class SummaryViewComponent : ViewComponent
+    {
+        private readonly DomainNotificationHandler _notifications;
+
+        public SummaryViewComponent(INotificationHandler<DomainNotification> notifications) {
+            _notifications = (DomainNotificationHandler)notifications;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() {
+            var notifications = await Task.FromResult((_notifications.GetNotifications()));
+            notifications.ForEach(n => ViewData.ModelState.AddModelError(string.Empty, n.Value));
+
+            return View();
+        }
+    }
+}
