0882a25ec7bf83010a551ce63862fe53a7705c3e
Qi Xiang <qixyqix@outlook.com>
2020-01-11 17:07:11 +0800

    EquinoxCore.Web added Index added index homecontroller

--- EquinoxCore.Web/Controllers/HomeController.cs
+++ EquinoxCore.Web/Controllers/HomeController.cs
@@ -9,29 +9,17 @@ using EquinoxCore.Web.Models;
 
 namespace EquinoxCore.Web.Controllers
 {
+    [Route("home")]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
+        [Route("welcome")]
+        [Route("")]
+        [Route("/")]
+        public IActionResult Index() {
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
