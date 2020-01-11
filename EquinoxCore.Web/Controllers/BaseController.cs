428372e92b601cfc1a82fbd4bd6fed7361796330
Qi Xiang <qixyqix@outlook.com>
2020-01-11 18:15:45 +0800

    EquinoxCore.Web added basecontroller

---
+++ EquinoxCore.Web/Controllers/BaseController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EquinoxCore.Domain.Core.Notifications;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EquinoxCore.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        private readonly DomainNotificationHandler _notifications;
+
+        public BaseController(INotificationHandler<DomainNotification> notifications) {
+            _notifications = (DomainNotificationHandler)notifications;
+        }
+
+        public bool IsValidOperation()
+        {
+            return (!_notifications.HasNotifications());
+        }
+    }
+}
